272f8bad26c05ff5a0f0112568ac506a6b34898c
vuthihatrang <vthtrang09@gmail.com>
2021-07-17 19:15:52 +0700

    Init new automation test framework

---
+++ actions/commos/BaseTest.java
@@ -0,0 +1,5 @@
+package commos;
+
+public class BaseTest {
+
+}
---
+++ testdata/com/bankguru/data/Payment.java
@@ -0,0 +1,14 @@
+package com.bankguru.data;
+
+public class Payment {
+	public class FundTransfer{
+		
+		
+	}
+	public class Deposit{
+		
+	}
+	public class Withdrawal{
+		
+	}
+}
